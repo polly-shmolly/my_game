9003741495297504cb17b8bb08c8a4460fa7a90c
polly-shmolly <polly.tsarik@gmail.com>
2023-03-02 11:52:25 +0300

    #6-3 landing particles

--- level.py
+++ level.py
@@ -16,6 +16,7 @@ class Level:
 
         # dust
         self.dust_sprite = pygame.sprite.GroupSingle()
+        self.player_on_ground = False
 
     def create_jump_particles(self, pos):
         if self.player.sprite.facing_right:
@@ -24,6 +25,21 @@ class Level:
             pos += pygame.math.Vector2(10, -5)
         jump_particle_sprite = ParticleEffect(pos, 'jump')
         self.dust_sprite.add(jump_particle_sprite)
+
+    def get_player_on_ground(self):
+        if self.player.sprite.on_ground:
+            self.player_on_ground = True
+        else:
+            self.player_on_ground = False
+
+    def create_landing_dust(self):
+        if not self.player_on_ground and self.player.sprite.on_ground and not self.dust_sprite.sprites():
+            if self.player.sprite.facing_right:
+                offset = pygame.math.Vector2(10, 15)
+            else:
+                offset = pygame.math.Vector2(-10, 15)
+            fall_dust_particles = ParticleEffect(self.player.sprite.rect.midbottom - offset, 'land')
+            self.dust_sprite.add(fall_dust_particles)
 
     def setup_level(self, layout):
         self.tiles = pygame.sprite.Group()
@@ -108,7 +124,9 @@ class Level:
         # player
         self.player.update()
         self.horizontal_movement_collision()
+        self.get_player_on_ground()
         self.vertical_movement_collision()
+        self.create_landing_dust()
         self.player.draw(self.display_surface)
 
 
