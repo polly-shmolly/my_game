76364910935489092e972d7fc686fd6a4c0f3e82
polly-shmolly <polly.tsarik@gmail.com>
2023-03-29 14:04:26 +0300

    #12 fixing the collision bugs

--- player.py
+++ player.py
@@ -24,6 +24,7 @@ class Player(pygame.sprite.Sprite):
 		self.speed = 8
 		self.gravity = 0.8
 		self.jump_speed = -16
+		self.collision_rect = pygame.Rect(self.rect.topleft, (50, self.rect.height))
 
 		# player status
 		self.status = 'idle'
@@ -61,9 +62,11 @@ class Player(pygame.sprite.Sprite):
 		image = animation[int(self.frame_index)]
 		if self.facing_right:
 			self.image = image
+			self.rect.bottomleft = self.collision_rect.bottomleft
 		else:
 			flipped_image = pygame.transform.flip(image, True, False)
 			self.image = flipped_image
+			self.rect.bottomright = self.collision_rect.bottomright
 
 		if self.invincible:
 			alpha = self.wave_value()
@@ -71,19 +74,7 @@ class Player(pygame.sprite.Sprite):
 		else:
 			self.image.set_alpha(255)
 
-		# set the rect
-		if self.on_ground and self.on_right:
-			self.rect = self.image.get_rect(bottomright=self.rect.bottomright)
-		elif self.on_ground and self.on_left:
-			self.rect = self.image.get_rect(bottomleft=self.rect.bottomleft)
-		elif self.on_ground:
-			self.rect = self.image.get_rect(midbottom=self.rect.midbottom)
-		elif self.on_ceiling and self.on_right:
-			self.rect = self.image.get_rect(topright=self.rect.topright)
-		elif self.on_ceiling and self.on_left:
-			self.rect = self.image.get_rect(topleft=self.rect.topleft)
-		elif self.on_ceiling:
-			self.rect = self.image.get_rect(midtop=self.rect.midtop)
+		self.rect = self.image.get_rect(midbottom=self.rect.midbottom)
 
 	def run_dust_animation(self):
 		if self.status == 'run' and self.on_ground:
@@ -130,7 +121,7 @@ class Player(pygame.sprite.Sprite):
 
 	def apply_gravity(self):
 		self.direction.y += self.gravity
-		self.rect.y += self.direction.y
+		self.collision_rect.y += self.direction.y
 
 	def jump(self):
 		self.direction.y = self.jump_speed
