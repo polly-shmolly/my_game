354d6b1f501adbc3c60023d9dd7b54a7cee49319
polly-shmolly <polly.tsarik@gmail.com>
2023-04-08 12:32:07 +0300

    #23 fix support

--- tests/test_support/test_fun.py
+++ tests/test_support/test_fun.py
@@ -3,7 +3,7 @@ from code.support import save_high_score
 
 def test_save_high_score():
     """:return data"""
-    t_save_data = save_high_score('polly', 5)
+    t_save_data = save_high_score('polly', 5, path_f='code/high_score.txt')
     expected = 'polly 5'
     assert t_save_data == expected
 
